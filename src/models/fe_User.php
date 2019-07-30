24c2fd2e450774c672d971d5e9f586c9724c0932
Lucas <yu0307@gmail.com>
2019-07-29 22:42:58 -0700

    PS

--- src/models/fe_User.php
+++ src/models/fe_User.php
@@ -7,11 +7,37 @@ use Illuminate\Database\Eloquent\Model;
 class fe_User extends \FeIron\Fe_Login\models\fe_users
 {
         // (string $related, string $name, string $table = null, string $foreignKey = null, string $otherKey = null)
-        public function roles(){
-            return $this->morphToMany('FeIron\Fe_Roles\models\fe_roles', 'target','fe_role_targets','role_id','role_id');
+        public function Roles(){
+            return $this->morphToMany('FeIron\Fe_Roles\models\fe_roles', 'target','fe_role_targets','target_id','role_id');
         }
 
         public function abilities(){
 
         }
+
+        public function rb(){
+            return $this->load('Roles.Abilities');
+        }
+
+        public function RoleAbilities(){
+            
+            return $abilities= $this->load('Roles.Abilities')->Roles->map(function($role){
+                return $role->Abilities;
+            })->flatten()->pluck('name','id');
+
+            $abilities->each;
+            // return $this->load('Roles.Abilities')->Roles->map(function($role){
+            //     return $role->Abilities->pluck('name','id')->all();
+            // });
+
+
+            // return $this->load('Roles.Abilities');
+            // return array_map(
+            //                 function($role){
+            //                     return $role->Abilities;
+            //                 },
+            //                 $this->load('Roles.Abilities')->Roles->all()
+            //                 );
+        }
+
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 Route::group(['namespace' => 'FeIron\Fe_Roles\Http\Controllers', 'middleware' => ['web']], function () {
     Route::get('testGuard',function(){
-        dd(Auth::user()->roles()->first());
+        // dd(FeIron\Fe_Roles\models\fe_User::find(1)->rb());
+        dd(FeIron\Fe_Roles\models\fe_User::find(1)->RoleAbilities());
+        dd(FeIron\Fe_Roles\models\fe_User::find(1)->all());
         // dd(get_class(Auth::user()));
         // dd(Config::get('auth'));
         // FeIron\Fe_Roles\models\fe_roles::find(1)->user()->first();
